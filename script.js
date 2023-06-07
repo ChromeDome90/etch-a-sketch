fac8b66235ab5165fc015f2e2064a425d7345f3a
Jake Young <jakeyoung@jakes-mbp-3.lan>
2023-06-07 11:42:04 -0600

    Add full functionality to game

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use strict"
 // Non-changing variables
-const DEFAULT_COLOR = '#CCDAD1'
+const DEFAULT_COLOR = '#172121'
 const DEFAULT_MODE = 'color'
 const DEFAULT_SIZE = 16
 
@@ -8,18 +8,18 @@ let currentColor = DEFAULT_COLOR
 let currentMode = DEFAULT_MODE
 let currentSize = DEFAULT_SIZE
 
-// Function to change color of drawing
+// Change color of drawing
 function setCurrentColor(newColor) {
     currentColor = newColor
 }
 
-// Function to change drawing mode
+// Change drawing mode
 function setCurrentMode(newMode) {
     activateButton(newMode)
     currentMode = newMode
 }
 
-// Function to set a new grid size
+// Set new grid size
 function setCurrentSize(newSize) {
     currentSize = newSize
 }
@@ -32,6 +32,85 @@ const sizeSlider = document.getElementById('sizeSlider')
 const resetBtn = document.getElementById('resetBtn')
 const grid = document.getElementById('grid')
 
+// Settings to change drawing style and erase canvas
 colorPicker.oninput = (e) => setCurrentColor(e.target.value)
+colorBtn.onclick = () => setCurrentMode('color')
+rainbowBtn.onclick = () => setCurrentMode('rainbow')
+eraserBtn.onclick = () => setCurrentMode('eraser')
+resetBtn.onclick = () => resetGrid()
+sizeSlider.onmousemove = (e) => updateSizeValue(e.target.value)
+sizeSlider.onchange = (e) => changeCanvasSize(e.target.value)
 
+// Allows user to click and drag to draw
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
 
+function changeCanvasSize(value) {
+    setCurrentSize(value)
+    updateSizeValue(value)
+    resetGrid()
+}
+
+function updateSizeValue(value) {
+    sizeValue.innerHTML = `${value} x ${value}`
+}
+
+function resetGrid() {
+    clearGrid()
+    setupGrid(currentSize)
+  }
+  
+function clearGrid() {
+    grid.innerHTML = ''
+  }
+  
+function setupGrid(size) {
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
+  
+    for (let i = 0; i < size * size; i++) {
+      const gridElement = document.createElement('div')
+      gridElement.classList.add('grid-element')
+      gridElement.addEventListener('mouseover', changeColor)
+      gridElement.addEventListener('mousedown', changeColor)
+      grid.appendChild(gridElement)
+    }
+  }
+  
+function changeColor(e) {
+    if (e.type === 'mouseover' && !mouseDown) return
+    if (currentMode === 'rainbow') {
+      const randomR = Math.floor(Math.random() * 256)
+      const randomG = Math.floor(Math.random() * 256)
+      const randomB = Math.floor(Math.random() * 256)
+      e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`
+    } else if (currentMode === 'color') {
+      e.target.style.backgroundColor = currentColor
+    } else if (currentMode === 'eraser') {
+      e.target.style.backgroundColor = '#CCDAD1'
+    }
+  }
+  
+function activateButton(newMode) {
+    if (currentMode === 'rainbow') {
+      rainbowBtn.classList.remove('active')
+    } else if (currentMode === 'color') {
+      colorBtn.classList.remove('active')
+    } else if (currentMode === 'eraser') {
+      eraserBtn.classList.remove('active')
+    } 
+  
+    if (newMode === 'rainbow') {
+      rainbowBtn.classList.add('active')
+    } else if (newMode === 'color') {
+      colorBtn.classList.add('active')
+    } else if (newMode === 'eraser') {
+      eraserBtn.classList.add('active')
+    }
+}
+  
+window.onload = () => {
+    setupGrid(DEFAULT_SIZE)
+    activateButton(DEFAULT_MODE)
+}
